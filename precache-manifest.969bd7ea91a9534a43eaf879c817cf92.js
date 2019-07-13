b8a9830777d7f952a1c7727e63be393514eb1af9
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-07-13 13:22:56 -0700

    Updates

---
+++ precache-manifest.969bd7ea91a9534a43eaf879c817cf92.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f39e813f1d8640a6852f2891927c653",
+    "url": "/19-Clicky-Game/index.html"
+  },
+  {
+    "revision": "f901a67ae079bf3a842a",
+    "url": "/19-Clicky-Game/static/js/2.16c4321e.chunk.js"
+  },
+  {
+    "revision": "4d445e0151d404f548a1",
+    "url": "/19-Clicky-Game/static/js/main.c2634bc7.chunk.js"
+  },
+  {
+    "revision": "650694a34334bd1d8c4e",
+    "url": "/19-Clicky-Game/static/js/runtime~main.5d2f718e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/19-Clicky-Game/precache-manifest.969bd7ea91a9534a43eaf879c817cf92.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/19-Clicky-Game/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
